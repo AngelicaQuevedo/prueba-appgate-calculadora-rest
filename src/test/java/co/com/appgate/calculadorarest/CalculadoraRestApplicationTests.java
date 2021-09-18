c8445b5b5fc31135b21b38dde7f090ec94e15eb1
AngelicaQuevedo <angelicaquevedo27@gmail.com>
2021-09-18 12:52:15 -0500

    Initial files

---
+++ src/test/java/co/com/appgate/calculadorarest/CalculadoraRestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.appgate.calculadorarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
